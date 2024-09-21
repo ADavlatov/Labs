29e575e25fdfbbac010d03c608fddd341c3d6bb4
ADavlatov <a@davlatov.space>
2024-09-21 14:52:47 +0300

    Лабораторная работа 2. Задание 1

---
+++ Lab2/Lab2Task1.py
@@ -0,0 +1,17 @@
+# Лабораторная работа №2. Задание №1
+# БВТ2403 - Давлатов Амирхан
+
+# Приветствие пользователя
+def greet(name):
+    print(f"Привет, {name}!")
+
+
+# Вычисление квадрата числа
+def square(number):
+    return number ** 2
+
+
+# Нахождение большего из двух чисел
+def max_of_two(num1, num2):
+    return num1 if num1 > num2 else num2
+
